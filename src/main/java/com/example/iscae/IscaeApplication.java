e1ec39f327fbc4101386579d613910adc01fd36a
zeighoum <hassen.zeighm@gmail.com>
2021-12-19 13:00:28 +0100

    first commit

---
+++ src/main/java/com/example/iscae/IscaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.iscae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iscae/IscaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iscae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b7dda21f0535ba4822a04766dffbb90410110399
zeighoum <hassen.zeighm@gmail.com>
2021-12-21 09:54:22 +0100

    add the exam class

--- src/main/java/com/example/iscae/entity/Exam.java
+++ src/main/java/com/example/iscae/entity/Exam.java
@@ -1,2 +1,28 @@
-package com.example.iscae.entity;public class Exam {
+package com.example.iscae.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Exam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date date;
+    private String Description;
+    private Date duree;
+
+    @ManyToOne
+    private Matiere matiere;
+    @ManyToOne
+    private Classe classe;
+
 }
